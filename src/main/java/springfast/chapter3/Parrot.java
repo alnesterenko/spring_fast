39b019b4b92d3db458f4ea36baeda68cf83c048a
alnesterenko <alzpupkin@gmail.com>
2025-05-13 17:53:49 +0300

    Установка связи между бинами. Initial commit.

---
+++ src/main/java/springfast/chapter3/Parrot.java
@@ -0,0 +1,27 @@
+package springfast.chapter3;
+
+public class Parrot {
+
+    private String name;
+
+    public Parrot() {
+    }
+
+    public Parrot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Parrot: " + name;
+    }
+}
---
+++ src/main/java/springfast/chapter3/Person.java
@@ -0,0 +1,24 @@
+package springfast.chapter3;
+
+public class Person {
+
+    private String name;
+
+    private Parrot parrot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+}
