000e1924b37642603598cde66399ed7e4857ede5
alnesterenko <alzpupkin@gmail.com>
2025-05-14 17:06:19 +0300

    3.2.3. Внедрение зависимости через сеттер

--- src/main/java/springfast/chapter3/Person.java
+++ src/main/java/springfast/chapter3/Person.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /*
-* Теперь поле parrot можно сделать final
+* У этого способа больше недостатков, чем преимуществ:
+*  код сложнее читать,
+*  нельзя пометить поле как final,
+*  тестирование не становится проще.
 * */
 
 @Component
@@ -12,11 +15,9 @@ public class Person {
 
     private String name = "Ella";
 
-    private final Parrot parrot;
+    private Parrot parrot;
 
-    @Autowired
-    public Person(Parrot parrot) {
-        this.parrot = parrot;
+    public Person() {
     }
 
     public String getName() {
@@ -29,5 +30,10 @@ public class Person {
 
     public Parrot getParrot() {
         return parrot;
+    }
+
+    @Autowired
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
     }
 }
