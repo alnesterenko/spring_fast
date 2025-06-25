124c86439edb5a148cc4f03c621a684ab7c1e04e
alnesterenko <alzpupkin@gmail.com>
2025-06-25 18:52:03 +0300

    Вторая попытка слить две ветки в одну.

--- src/main/java/springfast/chapter4/springcommentservice/repositories/DBCommentRepository.java
+++ src/main/java/springfast/chapter4/springcommentservice/repositories/DBCommentRepository.java
@@ -1,9 +1,7 @@
 package springfast.chapter4.springcommentservice.repositories;
 
-import org.springframework.stereotype.Component;
 import springfast.chapter4.springcommentservice.model.Comment;
 
-@Component
 public class DBCommentRepository implements CommentRepository {
 
     @Override
