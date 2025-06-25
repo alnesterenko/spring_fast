f7c5c932fbd9c6443e1f23a26dc9448fd3d000c8
alnesterenko <alzpupkin@gmail.com>
2025-06-25 18:28:11 +0300

    4.2. CommentService с использованием Spring. C @Autowired. С использованием @Component.

--- src/main/java/springfast/chapter4/springcommentservice/services/CommentService.java
+++ src/main/java/springfast/chapter4/springcommentservice/services/CommentService.java
@@ -1,5 +1,6 @@
 package springfast.chapter4.springcommentservice.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import springfast.chapter4.springcommentservice.model.Comment;
 import springfast.chapter4.springcommentservice.proxies.CommentNotificationProxy;
@@ -8,18 +9,10 @@ import springfast.chapter4.springcommentservice.repositories.CommentRepository;
 @Component
 public class CommentService {
 
-    private final CommentRepository commentRepository;
-
-    private final CommentNotificationProxy commentNotificationProxy;
-
-    /*
-     * !!! Если в классе только один конструктор, то анотация @Autowired не обязательна !!!
-     * */
-    public CommentService(CommentRepository commentRepository,
-                          CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
-    }
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
 
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
