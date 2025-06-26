7e69b8c81818a322b979f806f826903812fc0e84
alnesterenko <alzpupkin@gmail.com>
2025-06-26 21:11:16 +0300

    Бессмысленный коммит_2

--- src/main/java/springfast/chapter4/springcommentservice/main/Main.java
+++ src/main/java/springfast/chapter4/springcommentservice/main/Main.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import springfast.chapter4.springcommentservice.configuration.ProjectConfiguration;
 import springfast.chapter4.springcommentservice.model.Comment;
 import springfast.chapter4.springcommentservice.services.CommentService;
-
+/* Бессмысленный коммит_2 */
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
