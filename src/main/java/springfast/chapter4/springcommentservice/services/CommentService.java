144015460fce5aa36a9292d54214c2f205c86d2b
alnesterenko <alzpupkin@gmail.com>
2025-06-26 19:36:43 +0300

    Бессмысленный коммит_1

--- src/main/java/springfast/chapter4/springcommentservice/services/CommentService.java
+++ src/main/java/springfast/chapter4/springcommentservice/services/CommentService.java
@@ -1,17 +1,24 @@
 package springfast.chapter4.springcommentservice.services;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import springfast.chapter4.springcommentservice.model.Comment;
 import springfast.chapter4.springcommentservice.proxies.CommentNotificationProxy;
 import springfast.chapter4.springcommentservice.repositories.CommentRepository;
 
+@Component
 public class CommentService {
 
     private final CommentRepository commentRepository;
 
     private final CommentNotificationProxy commentNotificationProxy;
 
+    /*
+     * !!! Если в классе только один конструктор, то анотация @Autowired не обязательна !!!
+     * */
     public CommentService(CommentRepository commentRepository,
-                          CommentNotificationProxy commentNotificationProxy) {
+                          /* Каждый параметр, для которого нужно использовать специальную реализацию, сопровождается аннотацией @Qualifier */
+                          @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
